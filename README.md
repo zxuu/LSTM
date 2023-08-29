ca9766567224191e57c72d308e6aac93e53f628e
zxuu <z18255713582@outlook.com>
2023-08-29 17:48:03 +0800

    单层LSTM_cell效果

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LSTM
 实现LSTM内部结构
+> **框架图**
+
+![lstm框架图](./img/LSTM%E6%A1%86%E6%9E%B6.png)
+![框架图2](./img/%E6%A1%86%E6%9E%B6%E5%9B%BE2.png)
+>**多层结构**
+
+![多层结构](./img/%E5%A4%9A%E5%B1%82.webp)
+
+```bash
+layer=1的LSTM就比layer=3的RNN高了大约三个百分点
+all_sentence acc:0.5989, loss:116.9872
+all_sentence acc:0.5999, loss:116.8669
+all_sentence acc:0.5999, loss:116.8870
+all_sentence acc:0.6016, loss:116.7693
+```
---
+++ lstm.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+class myLstm(nn.Module):
+    def __intit(self,input_sz,hidden_sz):
+        super().__init__()
+        self.input_size=input_sz
+        self.hidden_size=hidden_sz
+
+        #input_gate
+        self.U_i=nn.Parameter(torch.Tensor(input_sz,hidden_sz))
+        self.V_i = nn.Parameter(torch.Tensor(hidden_sz,hidden_sz))
+        self.b_i = nn.parameter(torch.Tensor(hidden_sz))
+
+        #forget_gate
+        self.U_f = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_f = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_f = nn.Parameter(torch.Tensor(hidden_sz))
+    
+        #c_t
+        self.U_c = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_c = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_c = nn.Parameter(torch.Tensor(hidden_sz))
+    
+        #output_gate
+        self.U_o = nn.Parameter(torch.Tensor(input_sz, hidden_sz))
+        self.V_o = nn.Parameter(torch.Tensor(hidden_sz, hidden_sz))
+        self.b_o = nn.Parameter(torch.Tensor(hidden_sz))
+    
+        self.init_weights()
+    def forward(self,x,init_states=None):
+        bs,seq_sz,_=x.size()
+        hidden_seq=[]
+
+        if init_states is None:
+            h_t,c_t=(
+                torch.zeros(bs,self.hidden_size).to(x.device),
+                torch.zeros(bs,self.hidden_size).to(x.device)
+            )
+        else:
+            h_t, c_t = init_states
+        for t in range(seq_sz):
+            x_t = x[:, t, :]
+
+            i_t = torch.sigmoid(x_t @ self.U_i + h_t @ self.V_i + self.b_i)
+            f_t = torch.sigmoid(x_t @ self.U_f + h_t @ self.V_f + self.b_f)
+            g_t = torch.tanh(x_t @ self.U_c + h_t @ self.V_c + self.b_c)
+            o_t = torch.sigmoid(x_t @ self.U_o + h_t @ self.V_o + self.b_o)
+            c_t = f_t * c_t + i_t * g_t
+            h_t = o_t * torch.tanh(c_t)
+
+            hidden_seq.append(h_t.unsqueeze(0))
+        hidden_seq = torch.cat(hidden_seq, dim=0)
+        hidden_seq = hidden_seq.transpose(0, 1).contiguous()
+        return hidden_seq, (h_t, c_t)
+    def init_weights():
+        pass
